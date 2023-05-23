ae874eb0222cd733c641b1cc9a332d47c7660584
MamiyaXR <xhq2012n@gmail.com>
2023-05-23 09:55:59 +0800

    BindablePropertyTool

---
+++ Assets/MamiyaTool/Script/BindablePropertyTool/EditorPrefsBoolProperty.cs
@@ -0,0 +1,12 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace MamiyaTool {
+    public class EditorPrefsBoolProperty : BindableProperty<bool> {
+        public EditorPrefsBoolProperty(string key, bool initValue = false) {
+            mValue = EditorPrefs.GetBool(key, initValue);
+            Register(value => { EditorPrefs.SetBool(key, value); });
+        }
+    }
+}
+#endif
---
+++ Assets/MamiyaTool/Script/BindablePropertyTool/PlayerPrefsFloatProperty.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MamiyaTool {
+    public class PlayerPrefsFloatProperty : BindableProperty<float> {
+        public PlayerPrefsFloatProperty(string saveKey, float defaultValue = 0f) {
+            mValue = PlayerPrefs.GetFloat(saveKey, defaultValue);
+            Register(value => PlayerPrefs.SetFloat(saveKey, value));
+        }
+    }
+}
